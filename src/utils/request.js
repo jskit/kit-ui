d13b6b3e011968cb6036a11c281a61de95f12742
jack <1395093509@qq.com>
2017-11-04 12:58:09 +0800

    test

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,19 +1,20 @@
 // 此文件建议跟着项目走，可随时变更修改、扩展定制
 
-import fetch from 'kit-fetch';
-import { notification } from 'kit-ui';
+require('es6-promise').polyfill()
+import fetch from 'kit-fetch'
+import { Notification } from 'kit-ui'
 
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
-    return response;
+    return response
   }
-  notification.error({
+  Notification.error({
     message: `请求错误 ${response.status}: ${response.url}`,
     description: response.statusText,
-  });
-  const error = new Error(response.statusText);
-  error.response = response;
-  throw error;
+  })
+  const error = new Error(response.statusText)
+  error.response = response
+  throw error
 }
 
 /**
@@ -23,36 +24,36 @@ function checkStatus(response) {
  * @param  {object} [options] The options we want to pass to "fetch"
  * @return {object}           An object containing either "data" or "err"
  */
-export default function request(url, options) {
+export default async function request(url, options) {
   const defaultOptions = {
     credentials: 'include',
-  };
-  const newOptions = { ...defaultOptions, ...options };
+  }
+  const newOptions = { ...defaultOptions, ...options }
   if (newOptions.method === 'POST' || newOptions.method === 'PUT') {
     newOptions.headers = {
       Accept: 'application/json',
       'Content-Type': 'application/json; charset=utf-8',
       ...newOptions.headers,
-    };
-    newOptions.body = JSON.stringify(newOptions.body);
+    }
+    newOptions.body = JSON.stringify(newOptions.body)
   }
 
-  return fetch(url, newOptions)
+  return await fetch(url, newOptions)
     .then(checkStatus)
     .then(response => response.json())
     .catch((error) => {
       if (error.code) {
-        notification.error({
+        Notification.error({
           message: error.name,
           description: error.message,
-        });
+        })
       }
       if ('stack' in error && 'message' in error) {
-        notification.error({
+        Notification.error({
           message: `请求错误: ${url}`,
           description: error.message,
-        });
+        })
       }
-      return error;
-    });
+      return error
+    })
 }
