608847105ee59a02932e78fd05284aafed5c0c6f
jack <1395093509@qq.com>
2017-11-09 10:55:23 +0800

    更新

--- packages/webpack.packages.js
+++ packages/webpack.packages.js
@@ -1,18 +1,23 @@
+var fs = require('fs-extra')
 var path = require('path')
 var webpack = require('webpack')
 var merge = require('webpack-merge')
 var webpackBaseConfig = require('../build/webpack.base.conf.js')
+// node packages --out-dir lib
+
+// 清空 lib 目录
+fs.emptyDirSync(path.resolve(__dirname, '../lib'))
 
 process.env.NODE_ENV = 'production'
 
 module.exports = merge(webpackBaseConfig, {
   entry: {
-    main: './packages/index.js',
+    app: './packages/index.js',
   },
   output: {
     path: path.resolve(__dirname, '../lib'),
     publicPath: '/lib',
-    filename: 'kit-ui.js',
+    filename: 'index.js',
     library: 'kit-ui',
     libraryTarget: 'umd',
     umdNamedDefine: true,
